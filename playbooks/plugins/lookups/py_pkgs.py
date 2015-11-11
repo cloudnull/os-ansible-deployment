a39100157881bc0eda2eac2c5359e4593be36e57
Kevin Carter <kevin.carter@rackspace.com>
2015-11-11 10:32:03 -0600

    Functional backport for the new repo-build process
    The change backports the repo-build role into kilo such that
    we're allowing the Kilo build process to benifit from the changes
    we've made upstream. This will also allow for the kilo playbooks
    to clone from a repo using a generated manifest file. This will
    ensure that the build process is more repatable, creating an
    environment with faster build times, and dropping our dependency
    on the yaprt library.
    
    This change brings with it all of the assosiated updates that have been
    made to the various plugins and playbooks to ensure a better repo-env.
    
    * In the change the repo-* plays have been backported from liberty
    * the repo-build role has been backported
    * the py_pkg lookup plugin changes has been backported
    
    Change-Id: I5a3a1506bf3df8b5812d9131248d0156a8dd7dd6
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

--- playbooks/plugins/lookups/py_pkgs.py
+++ playbooks/plugins/lookups/py_pkgs.py
@@ -15,6 +15,7 @@
 # (c) 2014, Kevin Carter <kevin.carter@rackspace.com>
 
 import os
+import re
 import traceback
 
 from ansible import errors
@@ -22,14 +23,16 @@ from ansible import utils
 import yaml
 
 
-VERSION_DESCRIPTORS = ['>=', '<=', '==', '!=', '<', '>']
+# Used to keep track of git package parts as various files are processed
+GIT_PACKAGE_DEFAULT_PARTS = dict()
 
 
 REQUIREMENTS_FILE_TYPES = [
-    'requirements.txt',
     'global-requirements.txt',
     'test-requirements.txt',
-    'dev-requirements.txt'
+    'dev-requirements.txt',
+    'requirements.txt',
+    'global-requirement-pins.txt'
 ]
 
 
@@ -43,6 +46,102 @@ BUILT_IN_PIP_PACKAGE_VARS = [
 ]
 
 
+def git_pip_link_parse(repo):
+    """Return a tuple containing the parts of a git repository.
+
+    Example parsing a standard git repo:
+      >>> git_pip_link_parse('git+https://github.com/username/repo-name@tag')
+      ('repo-name',
+       'tag',
+       None,
+       'https://github.com/username/repo',
+       'git+https://github.com/username/repo@tag',
+       'repo_name')
+
+    Example parsing a git repo that uses an installable from a subdirectory:
+      >>> git_pip_link_parse(
+      ...     'git+https://github.com/username/repo@tag#egg=plugin.name'
+      ...     '&subdirectory=remote_path/plugin.name'
+      ... )
+      ('plugin.name',
+       'tag',
+       'remote_path/plugin.name',
+       'https://github.com/username/repo',
+       'git+https://github.com/username/repo@tag#egg=plugin.name&'
+       'subdirectory=remote_path/plugin.name',
+       'plugin.name')
+
+    :param repo: git repo string to parse.
+    :type repo: ``str``
+    :returns: ``tuple``
+    """'meta'
+
+    def _meta_return(meta_data, item):
+        """Return the value of an item in meta data."""
+
+        return meta_data.lstrip('#').split('%s=' % item)[-1].split('&')[0]
+
+    _git_url = repo.split('+')
+    if len(_git_url) >= 2:
+        _git_url = _git_url[1]
+    else:
+        _git_url = _git_url[0]
+
+    git_branch_sha = _git_url.split('@')
+    if len(git_branch_sha) > 2:
+        branch = git_branch_sha.pop()
+        url = '@'.join(git_branch_sha)
+    elif len(git_branch_sha) > 1:
+        url, branch = git_branch_sha
+    else:
+        url = git_branch_sha[0]
+        branch = 'master'
+
+    egg_name = name = os.path.basename(url.rstrip('/'))
+    egg_name = egg_name.replace('-', '_')
+
+    _branch = branch.split('#')
+    branch = _branch[0]
+
+    plugin_path = None
+    # Determine if the package is a plugin type
+    if len(_branch) > 1:
+        if 'subdirectory=' in _branch[-1]:
+            plugin_path = _meta_return(_branch[-1], 'subdirectory')
+            name = os.path.basename(plugin_path)
+
+        if 'egg=' in _branch[-1]:
+            egg_name = _meta_return(_branch[-1], 'egg')
+            egg_name = egg_name.replace('-', '_')
+
+        if 'gitname=' in _branch[-1]:
+            name = _meta_return(_branch[-1], 'gitname')
+
+    return name.lower(), branch, plugin_path, url, repo, egg_name
+
+
+def _pip_requirement_split(requirement):
+    """Split pip versions from a given requirement.
+
+    The method will return the package name, versions, and any markers.
+
+    :type requirement: ``str``
+    :returns: ``tuple``
+    """
+    version_descriptors = "(>=|<=|>|<|==|~=|!=)"
+    requirement = requirement.split(';')
+    requirement_info = re.split(r'%s\s*' % version_descriptors, requirement[0])
+    name = requirement_info[0]
+    marker = None
+    if len(requirement) > 1:
+        marker = requirement[1]
+    versions = None
+    if len(requirement_info) > 1:
+        versions = requirement_info[1]
+
+    return name, versions, marker
+
+
 class DependencyFileProcessor(object):
     def __init__(self, local_path):
         """Find required files.
@@ -53,13 +152,25 @@ class DependencyFileProcessor(object):
         self.pip = dict()
         self.pip['git_package'] = list()
         self.pip['py_package'] = list()
+        self.pip['role_packages'] = dict()
+        self.pip['git_data'] = list()
         self.git_pip_install = 'git+%s@%s'
         self.file_names = self._get_files(path=local_path)
 
         # Process everything simply by calling the method
-        self._process_files(ext=('yaml', 'yml'))
+        self._process_files()
 
-    def _filter_files(self, file_names, ext):
+    def _py_pkg_extend(self, packages):
+        for pkg in packages:
+            pkg_name = _pip_requirement_split(pkg)[0]
+            for py_pkg in self.pip['py_package']:
+                py_pkg_name = _pip_requirement_split(py_pkg)[0]
+                if pkg_name == py_pkg_name:
+                    self.pip['py_package'].remove(py_pkg)
+        self.pip['py_package'].extend(packages)
+
+    @staticmethod
+    def _filter_files(file_names, ext):
         """Filter the files and return a sorted list.
 
         :type file_names:
@@ -67,22 +178,14 @@ class DependencyFileProcessor(object):
         :returns: ``list``
         """
         _file_names = list()
+        file_name_words = ['/defaults/', '/vars/', '/user_']
+        file_name_words.extend(REQUIREMENTS_FILE_TYPES)
         for file_name in file_names:
             if file_name.endswith(ext):
-                if '/defaults/' in file_name or '/vars/' in file_name:
+                if any(i in file_name for i in file_name_words):
                     _file_names.append(file_name)
-                else:
-                    continue
-            elif os.path.basename(file_name) in REQUIREMENTS_FILE_TYPES:
-                with open(file_name, 'rb') as f:
-                    packages = [
-                        i.split()[0] for i in f.read().splitlines()
-                        if i
-                        if not i.startswith('#')
-                    ]
-                    self.pip['py_package'].extend(packages)
         else:
-            return sorted(_file_names, reverse=True)
+            return _file_names
 
     @staticmethod
     def _get_files(path):
@@ -106,24 +209,43 @@ class DependencyFileProcessor(object):
         :type git_data: ``dict``
         """
         for repo_plugin in git_repo_plugins:
+            strip_plugin_path = repo_plugin['package'].lstrip('/')
             plugin = '%s/%s' % (
                 repo_plugin['path'].strip('/'),
-                repo_plugin['package'].lstrip('/')
+                strip_plugin_path
             )
 
+            name = git_data['name'] = os.path.basename(strip_plugin_path)
+            git_data['egg_name'] = name.replace('-', '_')
             package = self.git_pip_install % (
-                git_data['repo'],
-                '%s#egg=%s&subdirectory=%s' % (
-                    git_data['branch'],
-                    repo_plugin['package'].strip('/'),
-                    plugin
-                )
+                git_data['repo'], git_data['branch']
             )
-
+            package += '#egg=%s' % git_data['egg_name']
+            package += '&subdirectory=%s' % plugin
+            package += '&gitname=%s' % name
             if git_data['fragments']:
-                package = '%s&%s' % (package, git_data['fragments'])
+                package += '&%s' % git_data['fragments']
 
+            self.pip['git_data'].append(git_data)
             self.pip['git_package'].append(package)
+
+            if name not in GIT_PACKAGE_DEFAULT_PARTS:
+                GIT_PACKAGE_DEFAULT_PARTS[name] = git_data.copy()
+            else:
+                GIT_PACKAGE_DEFAULT_PARTS[name].update(git_data.copy())
+
+    @staticmethod
+    def _check_defaults(git_data, name, item):
+        """Check if a default exists and use it if an item is undefined.
+
+        :type git_data: ``dict``
+        :type name: ``str``
+        :type item: ``str``
+        """
+        if not git_data[item] and name in GIT_PACKAGE_DEFAULT_PARTS:
+            check_item = GIT_PACKAGE_DEFAULT_PARTS[name].get(item)
+            if check_item:
+                git_data[item] = check_item
 
     def _process_git(self, loaded_yaml, git_item):
         """Process git repos.
@@ -133,52 +255,69 @@ class DependencyFileProcessor(object):
         """
         git_data = dict()
         if git_item.split('_')[0] == 'git':
-            var_name = 'git'
+            prefix = ''
         else:
-            var_name = git_item.split('_git_repo')[0]
+            prefix = '%s_' % git_item.split('_git_repo')[0].replace('.', '_')
 
-        git_data['repo'] = loaded_yaml.get(git_item)
-        git_data['branch'] = loaded_yaml.get(
-            '%s_git_install_branch' % var_name.replace('.', '_')
-        )
+        # Set the various variable definitions
+        repo_var = prefix + 'git_repo'
+        name_var = prefix + 'git_package_name'
+        branch_var = prefix + 'git_install_branch'
+        fragment_var = prefix + 'git_install_fragments'
+        plugins_var = prefix + 'repo_plugins'
 
-        if not git_data['branch']:
-            git_data['branch'] = loaded_yaml.get(
-                'git_install_branch',
-                'master'
+        # get the repo definition
+        git_data['repo'] = loaded_yaml.get(repo_var)
+
+        # get the repo name definition
+        name = git_data['name'] = loaded_yaml.get(name_var)
+        if not name:
+            name = git_data['name'] = os.path.basename(
+                git_data['repo'].rstrip('/')
             )
+        git_data['egg_name'] = name.replace('-', '_')
 
-        package = self.git_pip_install % (
-            git_data['repo'], git_data['branch']
-        )
+        # get the repo branch definition
+        git_data['branch'] = loaded_yaml.get(branch_var)
+        self._check_defaults(git_data, name, 'branch')
+        if not git_data['branch']:
+            git_data['branch'] = 'master'
 
-        git_data['fragments'] = loaded_yaml.get(
-            '%s_git_install_fragments' % var_name.replace('.', '_')
-        )
+        package = self.git_pip_install % (git_data['repo'], git_data['branch'])
+
+        # get the repo fragment definitions, if any
+        git_data['fragments'] = loaded_yaml.get(fragment_var)
+        self._check_defaults(git_data, name, 'fragments')
+
+        package += '#egg=%s' % git_data['egg_name']
+        package += '&gitname=%s' % name
         if git_data['fragments']:
-            package = '%s#%s' % (package, git_data['fragments'])
+            package += '&%s' % git_data['fragments']
 
         self.pip['git_package'].append(package)
+        self.pip['git_data'].append(git_data.copy())
 
-        git_repo_plugins = loaded_yaml.get('%s_repo_plugins' % var_name)
-        if git_repo_plugins:
+        # Set the default package parts to track data during the run
+        if name not in GIT_PACKAGE_DEFAULT_PARTS:
+            GIT_PACKAGE_DEFAULT_PARTS[name] = git_data.copy()
+        else:
+            GIT_PACKAGE_DEFAULT_PARTS[name].update()
+
+        # get the repo plugin definitions, if any
+        git_data['plugins'] = loaded_yaml.get(plugins_var)
+        self._check_defaults(git_data, name, 'plugins')
+        if git_data['plugins']:
             self._check_plugins(
-                git_repo_plugins=git_repo_plugins,
+                git_repo_plugins=git_data['plugins'],
                 git_data=git_data
             )
 
-    def _process_files(self, ext):
-        """Process files.
+    def _process_files(self):
+        """Process files."""
 
-        :type ext: ``tuple``
-        """
-        file_names = self._filter_files(
-            file_names=self.file_names,
-            ext=ext
-        )
-
-        for file_name in file_names:
-            with open(file_name, 'rb') as f:
+        role_name = None
+        for file_name in self._filter_files(self.file_names, ('yaml', 'yml')):
+            with open(file_name, 'r') as f:
                 # If there is an exception loading the file continue
                 #  and if the loaded_config is None continue. This makes
                 #  no bad config gets passed to the rest of the process.
@@ -190,15 +329,44 @@ class DependencyFileProcessor(object):
                     if not loaded_config:
                         continue
 
+                    if 'roles' in file_name:
+                        _role_name = file_name.split('roles%s' % os.sep)[-1]
+                        role_name = _role_name.split(os.sep)[0]
+
             for key, values in loaded_config.items():
-                if key.endswith('git_repo'):
-                    self._process_git(
-                        loaded_yaml=loaded_config,
-                        git_item=key
-                    )
+                # This conditional is set to ensure we're not processes git
+                #  repos from the defaults file which may conflict with what is
+                #  being set in the repo_packages files.
+                if '/defaults/main' not in file_name:
+                    if key.endswith('git_repo'):
+                        self._process_git(
+                            loaded_yaml=loaded_config,
+                            git_item=key
+                        )
 
                 if [i for i in BUILT_IN_PIP_PACKAGE_VARS if i in key]:
-                    self.pip['py_package'].extend(values)
+                    self._py_pkg_extend(values)
+
+                    if role_name:
+                        if role_name not in self.pip['role_packages']:
+                            self.pip['role_packages'][role_name] = values
+                        else:
+                            self.pip['role_packages'][role_name].extend(values)
+                            self.pip['role_packages'][role_name] = sorted(
+                                set(
+                                    self.pip['role_packages'][role_name]
+                                )
+                            )
+
+        for file_name in self._filter_files(self.file_names, 'txt'):
+            if os.path.basename(file_name) in REQUIREMENTS_FILE_TYPES:
+                with open(file_name, 'r') as f:
+                    packages = [
+                        i.split()[0] for i in f.read().splitlines()
+                        if i
+                        if not i.startswith('#')
+                    ]
+                    self._py_pkg_extend(packages)
 
 
 def _abs_path(path):
@@ -231,8 +399,14 @@ class LookupModule(object):
         if isinstance(terms, basestring):
             terms = [terms]
 
-        return_list = list()
+        return_data = {
+            'packages': set(),
+            'remote_packages': set(),
+            'remote_package_parts': list(),
+            'role_packages': dict()
+        }
         for term in terms:
+            return_list = list()
             try:
                 dfp = DependencyFileProcessor(
                     local_path=_abs_path(str(term))
@@ -247,27 +421,68 @@ class LookupModule(object):
                         traceback.format_exc()
                     )
                 )
-        else:
-            return_data = {
-                'packages': list(),
-                'remote_packages': list()
-            }
-            for file_name in sorted(set(return_list)):
-                is_url = file_name.startswith(('http:', 'https:', 'git+'))
-                if is_url:
-                    if '@' not in file_name:
-                        return_data['packages'].append(file_name)
+
+            for item in return_list:
+                if item.startswith(('http:', 'https:', 'git+')):
+                    if '@' not in item:
+                        return_data['packages'].add(item)
                     else:
-                        return_data['remote_packages'].append(file_name)
+                        git_parts = git_pip_link_parse(item)
+                        item_name = git_parts[-1]
+                        if not item_name:
+                            item_name = git_pip_link_parse(item)[0]
+
+                        for rpkg in list(return_data['remote_packages']):
+                            rpkg_name = git_pip_link_parse(rpkg)[-1]
+                            if not rpkg_name:
+                                rpkg_name = git_pip_link_parse(item)[0]
+
+                            if rpkg_name == item_name:
+                                return_data['remote_packages'].remove(rpkg)
+                                return_data['remote_packages'].add(item)
+                                break
+                        else:
+                            return_data['remote_packages'].add(item)
                 else:
-                    return_data['packages'].append(file_name)
+                    return_data['packages'].add(item)
             else:
-                return_data['packages'] = ' '.join(
-                    ['"%s"' % i for i in set(return_data['packages'])]
-                )
+                return_data['packages'] = set([
+                    i.lower() for i in return_data['packages']
+                ])
 
-                return_data['remote_packages'] = ' '.join(
-                    ['"%s"' % i for i in set(return_data['remote_packages'])]
+                keys = [
+                    'name',
+                    'version',
+                    'fragment',
+                    'url',
+                    'original',
+                    'egg_name'
+                ]
+                remote_pkg_parts = [
+                    dict(
+                        zip(
+                            keys, git_pip_link_parse(i)
+                        )
+                    ) for i in return_data['remote_packages']
+                ]
+                return_data['remote_package_parts'].extend(remote_pkg_parts)
+                return_data['remote_package_parts'] = list(
+                    dict(
+                        (i['name'], i)
+                        for i in return_data['remote_package_parts']
+                    ).values()
                 )
+                return_data['role_packages'].update(dfp.pip['role_packages'])
+        else:
+            # Sort everything within the returned data
+            for key, value in return_data.items():
+                if isinstance(value, (list, set)):
+                    return_data[key] = sorted(value)
+            return [return_data]
 
-                return [return_data]
+
+# Used for testing and debuging usage: `python plugins/lookups/py_pkgs.py ../`
+if __name__ == '__main__':
+    import sys
+    import json
+    print(json.dumps(LookupModule().run(terms=sys.argv[1:]), indent=4))
